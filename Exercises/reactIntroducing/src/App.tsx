61b0bbb798ca96b2afe177853a7da8c3dc325a1f
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-06-18 11:21:49 +0400

    Component yaratmaq

--- Exercises/reactIntroducing/src/App.tsx
+++ Exercises/reactIntroducing/src/App.tsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
-import logo from "./logo.svg";
+import User from "./components/User";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const test = 28;
-  const isAuth = false;
+
   return (
     <div className="App">
-      <h4>1+1</h4>
-      <h4> {1 + 1}</h4>
-      <h4>qurbanQurbanzade</h4>
-      <h4>{"qurbanQurbanzade".toUpperCase()}</h4>
-      <h4>{test}</h4>
-      <div>{isAuth ? <p>Qeydiyyat var</p> : <p>Qeydiyyat yoxdur</p>}</div>
+      <h4>App Component</h4>
+      <User />
+      <User />
+      <User />
     </div>
   );
 }
---
+++ Exercises/reactIntroducing/src/components/User.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default User;
