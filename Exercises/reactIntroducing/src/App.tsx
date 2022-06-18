f9aee60aa67b1bc6e432e00147f7004235a5e2a7
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-06-18 11:49:59 +0400

    Functional Component Example

--- Exercises/reactIntroducing/src/App.tsx
+++ Exercises/reactIntroducing/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import User from "./components/User";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App container">
       <h4 className="bg-primary">App Component</h4>
       <User />
+      <Navbar />
     </div>
   );
 }
---
+++ Exercises/reactIntroducing/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h3>User App</h3>
+    </div>
+  );
+}
+
+export default Navbar;
