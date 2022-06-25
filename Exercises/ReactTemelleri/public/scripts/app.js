413149c1296f03b87229faa41735e272b1736af8
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-06-25 11:33:30 +0400

    JSX Example

--- Exercises/ReactTemelleri/public/scripts/app.js
+++ Exercises/ReactTemelleri/public/scripts/app.js
@@ -2,6 +2,19 @@
 
 //JSX
 var root = document.querySelector("#root");
-var template = /*#__PURE__*/React.createElement("h1", null, "Hello World");
-ReactDOM.render(template, root); // terminala bunu yaziriq
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello World"), /*#__PURE__*/React.createElement("div", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")), "let template2 =", /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", {
+  id: "product-name"
+}, "name: Iphone"), /*#__PURE__*/React.createElement("p", {
+  id: "product-price"
+}, "price: 2000"), /*#__PURE__*/React.createElement("p", {
+  id: "product-description"
+}, "Description: Good phone")));
+var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", {
+  id: "product-name"
+}, "name: Iphone"), /*#__PURE__*/React.createElement("p", {
+  id: "product-price"
+}, "price: 2000"), /*#__PURE__*/React.createElement("p", {
+  id: "product-description"
+}, "Description: Good phone"));
+ReactDOM.render(template2, root); // terminala bunu yaziriq
 // npx babel src/app.js --out-file=public/scripts/app.js
--- Exercises/ReactTemelleri/src/app.js
+++ Exercises/ReactTemelleri/src/app.js
@@ -1,9 +1,32 @@
 //JSX
 let root = document.querySelector("#root");
 
-let template = <h1>Hello World</h1>;
+let template = (
+  <div>
+    <h1>Hello World</h1>
+    <div>Lorem, ipsum dolor.</div>
+    <ul>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+    </ul>
+    let template2 =
+    <div>
+      <h2 id="product-name">name: Iphone</h2>
+      <p id="product-price">price: 2000</p>
+      <p id="product-description">Description: Good phone</p>
+    </div>
+  </div>
+);
+let template2 = (
+  <div>
+    <h2 id="product-name">name: Iphone</h2>
+    <p id="product-price">price: 2000</p>
+    <p id="product-description">Description: Good phone</p>
+  </div>
+);
 
-ReactDOM.render(template, root);
+ReactDOM.render(template2, root);
 
 // terminala bunu yaziriq
 // npx babel src/app.js --out-file=public/scripts/app.js
