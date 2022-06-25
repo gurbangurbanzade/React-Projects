edebe254d86b88957f12a2d09f159bf596c1b4f7
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-06-25 11:04:57 +0400

    Babel Integrasiyasi
    Burada babel necə kodları əvvəlki versiya kodlarına çevirir və bunu manual və avto etmək yolu gosterilib

---
+++ Exercises/ReactTemelleri/public/scripts/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//JSX
+var root = document.querySelector("#root");
+var template = /*#__PURE__*/React.createElement("h1", null, "Hello World");
+ReactDOM.render(template, root); // terminala bunu yaziriq
+// npx babel src/app.js --out-file=public/scripts/app.js
---
+++ Exercises/ReactTemelleri/src/app.js
@@ -0,0 +1,9 @@
+//JSX
+let root = document.querySelector("#root");
+
+let template = <h1>Hello World</h1>;
+
+ReactDOM.render(template, root);
+
+// terminala bunu yaziriq
+// npx babel src/app.js --out-file=public/scripts/app.js
