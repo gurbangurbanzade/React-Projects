ca7d2a82b1cd163f40643b87ec04e64a5c91e7b4
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-07-27 13:59:11 +0400

    useEffekt sertli numune

--- Exercises/ReactUdemyIA/01MyApp/my-app/src/Components/06-UseEffect/HookSaygac.js
+++ Exercises/ReactUdemyIA/01MyApp/my-app/src/Components/06-UseEffect/HookSaygac.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 
 function HookSaygac() {
   const [saygac, setSaygac] = useState(0);
+  const [ad, setAd] = useState("");
+
   useEffect(() => {
     document.title = `${saygac}- defe kliklendi`;
-  });
+    console.log("deyisiklik oldu");
+  }, [saygac]);
   return (
     <div>
       HookSaygac
       <hr />
+      <input type="text" value={ad} onChange={(e) => setAd(e.target.value)} />
       <button onClick={() => setSaygac(saygac + 1)}>
         {saygac}-Defe kliklendi
       </button>
