959c6d869e799d456c82b81de45e47792794c95d
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-08-07 11:51:17 +0400

    Component Example

---
+++ Exercises/ReactYoutubeYB/firstapp/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import User from "./components/User";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>App Component</h4>
+      <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ Exercises/ReactYoutubeYB/firstapp/src/components/User.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class User extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <button>Gonder</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default User;
